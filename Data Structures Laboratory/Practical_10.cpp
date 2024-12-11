e2de09286f314cd06d1a4ecf4c523cb23a598254
Parth Shah <137877398+parthshah-dev@users.noreply.github.com>
2024-12-11 18:38:44 +0530

    Practical_10.cpp

--- Data Structures Laboratory/Practical_10.cpp
+++ Data Structures Laboratory/Practical_10.cpp
@@ -2,49 +2,54 @@
 In any language program mostly syntax error occurs due to unbalancing delimiter such as (),{},[]. Write C++ program using stack to check whether given expression is well parenthesized or not
 */
 
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-struct Stack{
-    int data[50];
-    int top = -1;
-};
+struct Stack {
+    char data[50];
+    int top;
+}s; 
 
-bool checker(struct Stack s){
+bool checker() {
     string str;
-    cout << "Enter a string: ";
+    cout << "Enter an expression: ";
     cin >> str;
 
-    for(int i=0; i<str.length(); i++){
-        if(str[i]=='(' || str[i]=='{' || str[i]=='['){
-            if(s.top==50){
-                cout << "\nStack Overflow!";
-            }
-            else{
-                s.top++;
-                s.data[s.top] = str[i];
-            }
-        }
-        else if(str[i]==')' && s.data[s.top]== '(' || str[i]=='}' && s.data[s.top]== '{' || str[i]==']' && s.data[s.top]== '['){
-            if(s.top==-1){
+    for (int i = 0; i < str.length(); i++) {
+        // Push opening delimiters onto the stack
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[') {
+            if (s.top >= 49) { // Stack is full
+                cout << "Stack overflow!" << endl;
                 return false;
             }
-            s.top--;
+            s.top++;
+            s.data[s.top] = str[i];
         }
-        else if(str[i]==')' || str[i]=='}' || str[i]=='}'){
-            return false;
+        // Check for closing delimiters
+        else if (str[i] == ')' || str[i] == '}' || str[i] == ']') {
+            if (s.top == -1) { // Stack is empty
+                return false;
+            }
+            char topChar = s.data[s.top];
+            s.top--; // Pop the stack
+            if ((str[i] == ')' && topChar != '(') ||
+                (str[i] == '}' && topChar != '{') ||
+                (str[i] == ']' && topChar != '[')) {
+                return false; // Mismatched delimiters
+            }
         }
     }
-    return s.top==-1;
+    return s.top == -1; // Stack should be empty if balanced
 }
-int main(){
-    struct Stack s;
+
+int main() {
+    s.top = -1; // Initialize stack with top as -1
     
-    if(checker(s)){
-        cout << "String is well parenthesized\n";
-    }else{
-        cout << "String is not well parenthesized\n";
+    if (checker()) {
+        cout << "The expression is well-parenthesized." << endl;
+    } else {
+        cout << "The expression is not well-parenthesized." << endl;
     }
     return 0;
 }
