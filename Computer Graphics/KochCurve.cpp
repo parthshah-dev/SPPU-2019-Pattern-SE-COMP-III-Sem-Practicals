f01a253a6f4e006b3699bd88ac0c89e0b2cc2523
Parth Shah <137877398+parthshah-dev@users.noreply.github.com>
2024-12-16 12:23:03 +0530

    KochCurve.cpp

--- Computer Graphics/KochCurve.cpp
+++ Computer Graphics/KochCurve.cpp
@@ -1,48 +1,42 @@
-#include<iostream>
-#include<graphics.h>
-#include<math.h>
+#include <iostream>
+#include <graphics.h>
+#include <math.h>
 using namespace std;
 
-class KochCurve{
-    public:
-    void koch(int it, int x1, int y1, int x5, int y5){
-        int x2, y2, x3, y3, x4, y4;
-        if(it==0){
-            line(x1,y1,x5,y5);
-        }
-        else{
-            delay(10);
-            int dx = (x5-x1)/3;
-            int dy = (y5-y1)/3;
-            
-            x2 = x1 + dx;
-            y2 = y1 + dy;
-
-            x3 = (int)(0.5*(x1+x5)+sqrt(3)*(y1-y5)/6);
-            y3 = (int)(0.5*(y1+y5)+sqrt(3)*(x5-x1)/6);
-
-            x4 = 2*dx+x1;
-            y4 = 2*dy+y1;
-
-            koch(it-1, x1, y1, x2, y2);
-            koch(it-1, x2, y2, x3, y3);
-            koch(it-1, x3, y3, x4, y4);
-            koch(it-1, x4, y4, x5, y5);
-        }
+void drawKochCurve(int depth, int x1, int y1, int x2, int y2) {
+    if (depth == 0) {
+        line(x1, y1, x2, y2); 
+        return;
     }
-};
-int main(){
-    KochCurve k;
-    int it; 
-    cout << "Enter number of iterations: ";
-    cin >> it;
 
-    int gd=DETECT, gm;
+    int dx = (x2 - x1) / 3;
+    int dy = (y2 - y1) / 3;
+
+    int x3 = x1 + dx;
+    int y3 = y1 + dy;
+
+    int x5 = x2 - dx;
+    int y5 = y2 - dy;
+
+    int x4 = (int)(0.5 * (x1 + x2) + sqrt(3) * (y2 - y1) / 6);
+    int y4 = (int)(0.5 * (y1 + y2) - sqrt(3) * (x2 - x1) / 6);
+
+    drawKochCurve(depth - 1, x1, y1, x3, y3);
+    drawKochCurve(depth - 1, x3, y3, x4, y4);
+    drawKochCurve(depth - 1, x4, y4, x5, y5);
+    drawKochCurve(depth - 1, x5, y5, x2, y2);
+}
+
+
+int main() {
+    int iterations;
+    cout << "Enter number of iterations: ";
+    cin >> iterations;
+
+    int gd = DETECT, gm;
     initgraph(&gd, &gm, NULL);
 
-    k.koch(it,150,20,20,280);
-    k.koch(it,280,280,150,20);
-    k.koch(it,20,280,280,280);
+    drawKochCurve(iterations, 100, 200, 500, 200);
 
     getch();
     closegraph();
