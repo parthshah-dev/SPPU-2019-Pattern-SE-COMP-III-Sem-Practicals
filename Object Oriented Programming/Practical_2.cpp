6c191cb9568aa557614ff02c82531ad32b9bf36c
Parth Shah <137877398+parthshah-dev@users.noreply.github.com>
2024-12-17 17:39:22 +0530

    Practical_2.cpp

--- Object Oriented Programming/Practical_2.cpp
+++ Object Oriented Programming/Practical_2.cpp
@@ -1,6 +1,5 @@
 /*
-Experiment Number 2 : Develop a program in C++ to create a database of
-student’s information system containing the following information: Name, Roll number, Class, Division, Date of Birth, Blood group, Contactaddress, Telephone number, Driving license no. and other. Construct the database with suitable member functions. Make use of constructor, default constructor, copy constructor, destructor, static member functions, friend class, this pointer, inline code and dynamic memory allocation operators-new and delete as well as exception handling.
+Develop a program in C++ to create a database of student’s information system containing the following information: Name, Roll number, Class, Division, Date of Birth, Blood group, Contactaddress, Telephone number, Driving license no. and other. Construct the database with suitable member functions. Make use of constructor, default constructor, copy constructor, destructor, static member functions, friend class, this pointer, inline code and dynamic memory allocation operators-new and delete as well as exception handling.
 */
 #include <iostream>
 #include <string>
